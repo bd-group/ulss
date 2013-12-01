dac7eba9fcc335be2106a36cacd410a3d6c932b6
cloud0920 <cloud0920@qq.com>
2013-12-01 10:56:43 +0800

    change the group to the date

--- ulss-data-statistics/src/ulss/data/statistics/PrintCount.java
+++ ulss-data-statistics/src/ulss/data/statistics/PrintCount.java
@@ -25,7 +25,6 @@ import org.apache.log4j.PropertyConfigurator;
  */
 public class PrintCount implements Runnable {
 
-    long group = 0L;
     public SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     static Logger logger = null;
 
@@ -43,8 +42,6 @@ public class PrintCount implements Runnable {
             } catch (InterruptedException ex) {
                 logger.error(ex, ex);
             }
-
-            group++;
 
             logger.info("print the count ");
             String dataDir = (String) RuntimeEnv.getParam(RuntimeEnv.DATA_DIR);
@@ -85,9 +82,9 @@ public class PrintCount implements Runnable {
                         String tmptime = dateFormat.format(d);
                         AtomicLong[] al = timeToCount.get(tmptime);
                         if (al != null) {
-                            bos.write((group + " " + mq + " " + tmptime + " count : " + al[0]).getBytes());
+                            bos.write((dtime + " " + mq + " " + tmptime + " count : " + al[0]).getBytes());
                         } else {
-                            bos.write((group + " " + mq + " " + tmptime + " count : 0").getBytes());
+                            bos.write((dtime + " " + mq + " " + tmptime + " count : 0").getBytes());
                         }
                         d.setHours(d.getHours() - 1);
                         bos.write('\n');
@@ -109,7 +106,7 @@ public class PrintCount implements Runnable {
                         }
                         dh.setHours(0);
                         String tmpd = dateFormat.format(dh);
-                        bos.write((group + " " + mq + " " + tmpd + " count : " + dcount).getBytes());
+                        bos.write((dtime + " " + mq + " " + tmpd + " count : " + dcount).getBytes());
                         bos.write('\n');
                         dh.setDate(dh.getDate() - 1);
                     }
